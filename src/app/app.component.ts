20338cf580d1f0c6ed195f362a2edcc7085450d9
santoshir <hemantbly.kumar@gmail.com>
2019-08-24 18:50:13 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directive5pm';
+ persons=["hemant","sumit","amit","suresh"];
+ teachers= [
+   {name:"hemant",age:23,add:"delhi"},
+   {name:"Sumit",age:53,add:"kanpur"},
+   {name:"Amit",age:93,add:"Lku"},
+   {name:"ramesh",age:73,add:"noida"},
+   {name:"ajay",age:33,add:"delhi"}
+ ];
+
+
+ isUserLoggedIn:boolean=true;
 }
